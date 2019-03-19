42376f3401f4ce33c4fee3560a11945d1fa18f6b
Rishabgh <rishabgh@in-Rishabgh.local>
2019-03-19 22:19:16 +0530

    stroeing value as BigDecimal instead of double

--- src/day2/Probability.java
+++ src/day2/Probability.java
@@ -3,29 +3,28 @@ package day2;
 import java.math.BigDecimal;
 
 class Probability {
-    private final double value;
+    private final BigDecimal chance;
     private final int UPPER_BOUND = 1;
     private final int LOWER_BOUND = 0;
 
     Probability(double value) throws InvalidProbabilityException {
         if (isOutsideBound(value)) throw new InvalidProbabilityException();
-        this.value = value;
+        this.chance = BigDecimal.valueOf(value);
     }
-
 
     private boolean isOutsideBound(double value){
         return value < LOWER_BOUND || value > UPPER_BOUND;
     }
 
     Probability not() throws InvalidProbabilityException {
-        BigDecimal probability = BigDecimal.valueOf(this.value);
         BigDecimal upperBound = BigDecimal.valueOf(UPPER_BOUND);
-        BigDecimal improbability = upperBound.subtract(probability);
+        BigDecimal improbability = upperBound.subtract(this.chance);
         return new Probability(improbability.doubleValue());
     }
 
     Probability and(Probability probability1) throws InvalidProbabilityException {
-        return new Probability(this.value * probability1.value);
+        BigDecimal and = this.chance.multiply(probability1.chance);
+        return new Probability(and.doubleValue());
     }
 
     Probability or(Probability probability) throws InvalidProbabilityException {
@@ -33,10 +32,11 @@ class Probability {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Probability)) return false;
-        Probability that = (Probability) o;
-        return Double.compare(that.value, value) == 0;
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Probability)) return false;
+        Probability that = (Probability) obj;
+        int comparedResult = that.chance.compareTo(this.chance);
+        return comparedResult == 0;
     }
 }
