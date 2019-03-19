e3b8c92d27a1a50316acaf3910e3101d5771e310
Rishabgh <rishabgh@in-Rishabgh.local>
2019-03-19 21:50:43 +0530

    Add or method

--- src/day2/Probability.java
+++ src/day2/Probability.java
@@ -1,38 +1,42 @@
 package day2;
 
-import java.util.Objects;
+import java.math.BigDecimal;
 
 class Probability {
-    private final double probability;
-    private final int UPPER_BOUND=1;
+    private final double value;
+    private final int UPPER_BOUND = 1;
+    private final int LOWER_BOUND = 0;
 
-    Probability(double probability) throws InvalidProbabilityException {
-        if (probability < 0 || probability > 1) throw new InvalidProbabilityException();
-        this.probability = probability;
+    Probability(double value) throws InvalidProbabilityException {
+        if (isOutsideBound(value)) throw new InvalidProbabilityException();
+        this.value = value;
     }
 
 
+    private boolean isOutsideBound(double value){
+        return value < LOWER_BOUND || value > UPPER_BOUND;
+    }
+
     Probability not() throws InvalidProbabilityException {
-        return new Probability(UPPER_BOUND - this.probability) ;
+        BigDecimal probability = BigDecimal.valueOf(this.value);
+        BigDecimal upperBound = BigDecimal.valueOf(UPPER_BOUND);
+        BigDecimal improbability = upperBound.subtract(probability);
+        return new Probability(improbability.doubleValue());
     }
 
     Probability and(Probability probability1) throws InvalidProbabilityException {
-
-
-        return new Probability(this.probability * probability1.probability);
+        return new Probability(this.value * probability1.value);
     }
 
+    Probability or(Probability probability) throws InvalidProbabilityException {
+        return this.not().and(probability.not()).not();
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Probability)) return false;
         Probability that = (Probability) o;
-        return Double.compare(that.probability, probability) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(probability);
+        return Double.compare(that.value, value) == 0;
     }
 }
--- test/day2/ProbabilityTest.java
+++ test/day2/ProbabilityTest.java
@@ -5,35 +5,45 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProbabilityTest {
+
     @Test
     void shouldNotThrowExceptionIfValueIsBetween0And1() {
-        assertDoesNotThrow(() -> new Probability(0.1));
+        assertDoesNotThrow(() -> new Probability(0.1D));
     }
 
     @Test
     void shouldTrowExceptionIfValueIsBelow0() {
-        assertThrows(InvalidProbabilityException.class, () -> new Probability(-0.1));
+        assertThrows(InvalidProbabilityException.class, () -> new Probability(-0.1D));
     }
 
     @Test
     void shouldTrowExceptionIfValueIsAbove1() {
-        assertThrows(InvalidProbabilityException.class, () -> new Probability(1.1));
+        assertThrows(InvalidProbabilityException.class, () -> new Probability(1.1D));
     }
 
     @Test
-    void shouldCalculateTheChanceOfEventNotHappening() throws InvalidProbabilityException {
-        Probability probability = new Probability(0.7);
+    void shouldReturnNotOfGivenProbability() throws InvalidProbabilityException {
+        Probability probability = new Probability(0.3D);
         Probability actual = probability.not();
-        Probability expected = new Probability(0.3);
+        Probability expected = new Probability(0.7D);
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldReturnTheCompoundProbabilityOfTwoEvents() throws InvalidProbabilityException {
-        Probability probability = new Probability(0.3);
-        Probability actual = probability.and(new Probability(0.2));
-        Probability expected = new Probability(0.06);
+    void shouldReturnTheAndOfTwoProbability() throws InvalidProbabilityException {
+        Probability probability1 = new Probability(0.3D);
+        Probability probability2 = new Probability(0.7D);
+        Probability actual = probability1.and(probability2);
+        Probability expected = new Probability(0.21D);
         assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldReturnTheOrOfTwoProbability() throws InvalidProbabilityException {
+        Probability probability1 = new Probability(0.5D);
+        Probability probability2 = new Probability(0.5D);
+        Probability actual = probability1.or(probability2);
+        Probability expected = new Probability(0.75D);
+        assertEquals(expected, actual);
+    }
 }
